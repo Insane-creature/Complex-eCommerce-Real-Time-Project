11e6b8694d45e20080216f87e67f94359dc4770d
Anshika Soni <anshikasoni357@gmail.com>
2025-01-21 22:34:08 +0530

    Modifying loosecoupling

--- src/main/java/com/loose/coupling/LooseCouplingExample.java
+++ src/main/java/com/loose/coupling/LooseCouplingExample.java
@@ -1,4 +1,4 @@
-package com.loose.coupling;
+ package com.loose.coupling;
 
 public class LooseCouplingExample {
     public static void main(String[] main){
--- src/main/java/com/loose/coupling/UserManager.java
+++ src/main/java/com/loose/coupling/UserManager.java
@@ -14,5 +14,7 @@ public class UserManager {
 
     public String getUserInfo(){
         return userDataProvider.getUserDetails();
+
+        
     }
 }
