129b246083855f2d7adf874267c7dceb49eedf65
Anshika Soni <anshikasoni357@gmail.com>
2025-01-22 19:42:53 +0530

    Bean creation

---
+++ src/main/java/car/App.java
@@ -0,0 +1,16 @@
+package car;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import car.example.bean.MyBean;
+
+public class App {
+    public static void main(String[] args) {
+        ApplicationContext context = new ClassPathXmlApplicationContext("applicationBeanContext.xml");
+
+        MyBean myBean = (MyBean) context.getBean("myBean");
+        System.out.println(myBean);
+
+    }
+}
---
+++ src/main/java/car/example/bean/MyBean.java
@@ -0,0 +1,28 @@
+package car.example.bean;
+
+public class MyBean {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+     
+    public void showMessage(){
+        System.out.println("Message: "+message);
+    }
+
+    @Override
+    public String toString() {
+        // StringBuilder sb = new StringBuilder();
+        // sb.append("MyBean{");
+        // sb.append("message=").append(message);
+        // sb.append('}');
+        // return sb.toString();
+
+        return "MyBean{" + 
+                "message= '" + message + '\'' +
+                '}';
+
+    }
+     
+}
